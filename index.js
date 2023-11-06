46b0b59975316199e85f7dfdba3fd34de8b4d645
Haya <aminovhaya@gmail.com>
2023-11-06 09:37:25 +0200

    cors policy bug in heroku

--- index.js
+++ index.js
@@ -2,8 +2,30 @@
 require("dotenv").config()
 require('./DL/db').connect();
 const express = require('express'),
-    app = express(),
-    port = process.env.PORT||3005;
+    app = express();
+
+
+//Cors Configuration - Start
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, PUT, PATCH, GET, DELETE"
+    )
+    return res.status(200).json({})
+  }
+  next()
+})
+//Cors Configuration - End
+
+
+
+ const   port = process.env.PORT||3005;
 
 const router=require('./Routes');
 
@@ -12,15 +34,6 @@ const router=require('./Routes');
 app.use(express.json()) 
 app.use(require('cors')())
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-    res.sendStatus(204);
-
-    next();
-  })
 
 app.use("/api", router); //IS EQUIVALENT TO app.use("/api", require('./Routes'));
 
