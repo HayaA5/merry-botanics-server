ef0a4c0e32d4810480d201bbefcc18d9ff06a44f
Haya <aminovhaya@gmail.com>
2023-11-06 09:29:28 +0200

    cors bug

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ app.use(express.json())
 app.use(require('cors')())
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", " https://merry-botanics-server-d0d59207f054.herokuapp.com/");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    res.sendStatus(204);
+
     next();
   })
 
