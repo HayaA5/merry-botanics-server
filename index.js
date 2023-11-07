2ba9f352d346a02361f26158409d79d072da5001
Haya <aminovhaya@gmail.com>
2023-11-07 10:17:24 +0200

    heroku bug

--- index.js
+++ index.js
@@ -4,7 +4,28 @@ require('./DL/db').connect();
 const express = require('express'),
     app = express();
 
-
+//
+const corsOptions = {
+    origin: [`${process.env.CLIENT_URL}`],
+    methods: "GET,HEAD,PUT,OPTIONS,POST,DELETE",
+    allowedHeaders: [
+      "Access-Control-Allow-Headers",
+      "Origin",
+      "X-Requested-With",
+      "Content-Type",
+      "Accept",
+      "Authorization",
+      "token",
+      "Access-Control-Request-Method",
+      "Access-Control-Request-Headers",
+      "Access-Control-Allow-Credentials",
+    ],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
+  app.use(cors(corsOptions));
+//
 //Cors Configuration - Start
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
