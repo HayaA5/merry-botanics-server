a70ecece1b06bf351c237a28b36d8258c3fc1ab5
Haya <aminovhaya@gmail.com>
2023-11-07 10:22:25 +0200

    bugs heroku-cors

--- index.js
+++ index.js
@@ -3,45 +3,24 @@ require("dotenv").config()
 require('./DL/db').connect();
 const express = require('express'),
     app = express();
+    const cors = require("cors");
 
-//
-const corsOptions = {
-    origin: [`${process.env.CLIENT_URL}`],
-    methods: "GET,HEAD,PUT,OPTIONS,POST,DELETE",
-    allowedHeaders: [
-      "Access-Control-Allow-Headers",
-      "Origin",
-      "X-Requested-With",
-      "Content-Type",
-      "Accept",
-      "Authorization",
-      "token",
-      "Access-Control-Request-Method",
-      "Access-Control-Request-Headers",
-      "Access-Control-Allow-Credentials",
-    ],
-    credentials: true,
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  };
-  app.use(cors(corsOptions));
-//
 //Cors Configuration - Start
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  )
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "POST, PUT, PATCH, GET, DELETE"
-    )
-    return res.status(200).json({})
-  }
-  next()
-})
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*")
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested, Content-Type, Accept Authorization"
+//   )
+//   if (req.method === "OPTIONS") {
+//     res.header(
+//       "Access-Control-Allow-Methods",
+//       "POST, PUT, PATCH, GET, DELETE"
+//     )
+//     return res.status(200).json({})
+//   }
+//   next()
+// })
 //Cors Configuration - End
 
 
@@ -65,7 +44,28 @@ app.use(require('cors')())
   //
 app.use("/api", router); //IS EQUIVALENT TO app.use("/api", require('./Routes'));
 
-
+//
+const corsOptions = {
+    origin: [`${process.env.CLIENT_URL}`],
+    methods: "GET,HEAD,PUT,OPTIONS,POST,DELETE",
+    allowedHeaders: [
+      "Access-Control-Allow-Headers",
+      "Origin",
+      "X-Requested-With",
+      "Content-Type",
+      "Accept",
+      "Authorization",
+      "token",
+      "Access-Control-Request-Method",
+      "Access-Control-Request-Headers",
+      "Access-Control-Allow-Credentials",
+    ],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
+  app.use(cors(corsOptions));
+//
 
 app.listen(port, () => console.log(`server is running => ${port}`))
 
