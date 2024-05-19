df80fdb12c75d127375a936041fcc0bb125e56b4
Haya <aminovhaya@gmail.com>
2024-05-19 09:08:00 +0300

    for netlify deployment

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+import serverless from "serverless-http";
+// import express, { Router } from "express";
 
 require("dotenv").config()
 require('./DL/db').connect();
 const express = require('express'),
-app = express(), cors = require("cors");
-const   port = process.env.PORT||3005;
-const router=require('./Routes');
+    app = express(), cors = require("cors");
+const port = process.env.PORT || 3005;
+const router = require('./Routes');
 
-app.use(express.json()) 
+app.use(express.json())
 app.use(cors())
 app.use("/api", router); //app.use("/api", require('./Routes'));
 
 app.listen(port, () => console.log(`server is running => ${port}`))
 
 
+export const handler = serverless(app);
