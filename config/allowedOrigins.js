23034caa4af82276f8bc5c5e8b470384be226a28
Haya <aminovhaya@gmail.com>
2024-07-08 09:50:27 +0300

    dploy rndr

---
+++ config/allowedOrigins.js
@@ -0,0 +1,6 @@
+const allowedOrigins = [
+    'http://localhost:3000'
+    // 'https://earnest-salamander-ad5604.netlify.app/'
+]
+// 'https://technotes.onrender.com'
+module.exports = allowedOrigins
--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 // import serverless from "serverless-http";
 // import express, { Router } from "express";
-
+//-------
+const corsOptions = require('./config/corsOptions')
+//---------
 require("dotenv").config()
 require('./DL/db').connect();
 const express = require('express'),
     app = express(), cors = require("cors");
+// const corsOptions = require('./config/corsOptions')
 const port = process.env.PORT || 3005;
 const router = require('./Routes');
+// const connectDB = require('./config/dbConn')
 
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cors())
 app.use("/api", router); //app.use("/api", require('./Routes'));
